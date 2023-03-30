593235ad37a39dd90c938a604e716aa7cba3dd75
Subha Das <65977621+5ubh4d45@users.noreply.github.com>
2023-03-30 19:35:04 +0530

    Replaced angular with  react on the frontend.

--- employee-management-frontend/src/App.js
+++ employee-management-frontend/src/App.js
@@ -1,29 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import ListEmployeeComponent from "./components/ListEmployeeComponent";
-import HeaderComponent from "./components/HeaderComponent";
-import FooterComponent from "./components/FooterComponent";
-import CreateEmployeeComponent from "./components/CreateEmployeeComponent";
 
 function App() {
-    return (
-        <div>
-            <Router>
-                <HeaderComponent />
-                <div className="container">
-                    <Routes> localhost:3000
-                        <Route path="/" element={<ListEmployeeComponent />} />
-                        <Route path="employees" element={<ListEmployeeComponent />} />
-                        <Route path="add-employee" element={<CreateEmployeeComponent />} />
-                    </Routes>
-                </div>
-                <FooterComponent />
-            </Router>
-        </div>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
-
-
 
 export default App;
